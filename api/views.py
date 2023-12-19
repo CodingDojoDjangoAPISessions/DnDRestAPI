dea29505fd3fdd1a58c3189d5540315bff5c80b3
Stuart-Yee <ki6fzb@gmail.com>
2023-12-18 20:30:29 -0800

    Fixing some changes

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import CharacterSerializer
 from characters.models import Character
+
 
 # Create your views here.
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from characters.models import Character
 
+
 def index(request):
     return render(request, 'index.html')
+
 
 def dashboard(request):
     all_the_characters = Character.objects.all()
